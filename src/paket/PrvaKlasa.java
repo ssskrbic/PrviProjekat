62eed196598a7685fff7a7f883eec36e3dd51ae8
Gimnazija 4 <Gimnazija 4@DESKTOP-UCG6S9O>
2023-09-14 17:37:03 +0200

    dodajem nesto se promenilo

--- src/paket/PrvaKlasa.java
+++ src/paket/PrvaKlasa.java
@@ -4,7 +4,12 @@ public class PrvaKlasa {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//prvi komentar
+		
+		
+		/*viselinijski
+		 * *komentar
+		 */
 	}
 
 }
